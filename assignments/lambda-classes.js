6dbc917d1b61a371db64367be92dfe6a3db8fb25
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-06-13 14:37:11 -0500

    Added Person and Instructor classes to lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,28 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attributes){
+        //props
+        this.name=attributes.name;
+        this.age=attributes.age;
+        this.location=attributes.location;
+    }
+    //Methods
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+class Instructor extends Person{
+    constructor(instAttributes){
+        super(instAttributes);
+        this.specialty=instAttributes.specialty;
+        this.favLanguage=instAttributes.favLanguage;
+        this.catchPhrase=instAttributes.catchPhrase;
+    }
+    demo(){
+        return `Today we are learning about {subject}.`;
+    }
+    grade(){
+        return `{student.name} receives a perfect score on {subject}`;
+    }
+}
